bf92ed34bc7d5b0caaee564d2a78cfc9c0cafd9a
unknown <214159@jnu.ac.kr>
2021-07-21 05:57:17 +0900

    Lecture 2 1.1v

--- Lecture 2/Problem 8.py
+++ Lecture 2/Problem 8.py
@@ -1,18 +1,21 @@
 # 5개의 숫자 입력, 순서대로 커진다면 YES, 아니라면 NO 출력.
 # 0이 입력되면 프로그램 종료.
 
-a = int(input())
-if a==0: exit()
-b = int(input())
-if b==0: exit()
-c = int(input())
-if c==0: exit()
-d = int(input())
-if d==0: exit()
-e = int(input())
-if e==0: exit()
+counter = 0
+t = []
 
-if (a<b) and (b<c) and (c<d) and (d<e):
+while True:
+    k = int(input())
+    if k==0:
+        exit()
+    else:
+        t.append(k)
+        counter += 1
+        if counter == 5:
+            break
+
+
+if (t[0]<t[1]) and (t[1]<t[2]) and (t[2]<t[3]) and (t[3]<t[4]):
     print('YES')
 else:
     print('NO')
--- Lecture 2/Problem 9.py
+++ Lecture 2/Problem 9.py
@@ -3,7 +3,14 @@
 
 n, m, a = list(map(int, input().split()))
 
-w = n//a + 1
-h = m//a + 1
+if n%a==0:
+    w = n//a
+else:
+    w = n//a + 1
+
+if m%a==0:
+    h = m//a
+else:
+    h = m//a + 1
 
 print(w*h)
